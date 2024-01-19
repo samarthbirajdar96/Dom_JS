b7951f270529da985a0cfe7bfef30244f9404b95
Samarth Birajdar <samarthbirajdar96@gmail.com>
2024-01-19 15:52:39 +0530

    dom assign

---
+++ script.js
@@ -0,0 +1,9 @@
+var divElement = document.getElementById("myDiv");
+divElement.style.color = "blue";            
+divElement.style.backgroundColor = "yellow"; 
+divElement.style.margin = "20px";          
+divElement.style.padding = "10px";          
+divElement.style.fontSize = "18px";         
+divElement.style.fontWeight = "bold";       
+divElement.style.height = "200px";          
+divElement.style.width = "300px";  
